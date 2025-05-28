ede11eba49e0fc0ad551c1571bb881974ccb29ca
unknown <maximamaiolo101@gmail.com>
2025-05-28 16:43:20 -0300

    Página para Error 404

---
+++ frontend/404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Gestión de Educativa</title>
+  <link rel="icon" type="image/png" sizes="32x32" href="img/favicon.png">
+  <!--<link rel="stylesheet" href="css/w3.css" />-->
+  <link rel="stylesheet" href="css/styles.css" />
+</head>
+
+<body>
+  <div class="div_index">
+    <header>
+      <h1>Eror 404</h2>
+    </header>
+    <b>Perdon, no pudimos encontrar la página :C</b>
+    <main class="main_index">
+      <a class="boton" href="index.html">Volver al Panel Principal</a> 
+    </main>
+  </div>
+
+</body>
+</html>
